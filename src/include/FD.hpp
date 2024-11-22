d4dac6131ca6bab8c1ad85ac98dc24e8bed55d40
davi.melonio <davi.melonio@eliis.fr>
2024-11-21 23:38:15 -0300

    removed some wrong files

--- src/include/FD.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../lib/SeisMath/include/SeisMath.hpp"
-
-vec1d<float> compute_2D(const vec1d<float>& property, uint16_t Nx, uint16_t Nz, uint16_t Nt,
-                        float dx, float dz, float dt, int pos0, int posf, const vec1d<float>& model,
-                        bool snap_bool, uint16_t snap_num);
-
